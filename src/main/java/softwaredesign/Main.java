7e1b57c153e1d58342a345a6215670da9456cfc6
Quinn <q.d.rutherford@student.vu.nl>
2021-02-12 08:30:40 -0500

    This is assignment 1.

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -3,5 +3,6 @@ package softwaredesign;
 public class Main {
     public static void main (String[] args){
         System.out.println("Welcome to Software Design");
+        System.out.println("This is assignment 1");
     }
 }
