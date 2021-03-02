c0759b794e63546aa6133bd87bd136814e1a472b
nsazhinov <n.sazhinov@student.vu.nl>
2021-03-02 13:25:25 +0100

    Set up JavaFX library for project along with a test GUI of the timer

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,11 +1,30 @@
 package softwaredesign;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
 import java.util.Stack;
 
-public class Main {
-    public static void main (String[] args){
-        GameManager game = new GameManager();
+public class Main extends Application{
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL url = new File("src/main/resources/timer.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 200));
+        primaryStage.show();
+    }
+
+    public static void main (String[] args){
+
+        GameManager game = new GameManager();
+        Application.launch(args);
         //Thread t1 = new Thread(new TimerClass(20));
         //t1.start();
 
@@ -25,6 +44,7 @@ public class Main {
         bufferStack.peek().printBuffer();
         bufferStack.push(new Buffer(bufferStack.peek(), "C"));
         bufferStack.peek().printBuffer();
+
     }
 
     public static void GameStateTest() {
