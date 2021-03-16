4e9fe80473b8a82d0214a49e0a2c12d26c91d406
Fortanet A <antonioforcap@gmail.com>
2021-03-16 20:24:36 +0100

    Created FileReader class to handle the parsing of the text file that contains initial puzzle information.

---
+++ src/main/java/softwaredesign/FileReader.java
@@ -0,0 +1,42 @@
+package softwaredesign;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileReader {
+
+    public String readFile(String fileName) throws IOException {
+        //System.out.println("getResourcesAsStream : " + fileName);
+        InputStream is = getFileFromResourceAsStream(fileName);
+        String output = inputStreamToString(is);
+        return output;
+    }
+
+    private InputStream getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+    }
+
+    public static String inputStreamToString(InputStream i) throws IOException {
+        String fileContent = "";
+        BufferedReader r = new BufferedReader(new InputStreamReader(i));
+        String l;
+        while((l = r.readLine()) != null) {
+            //System.out.println("LINE: "+l);
+            fileContent += l + "\n";
+        }
+        i.close();
+        return fileContent;
+    }
+}
--- src/main/java/softwaredesign/Puzzle.java
+++ src/main/java/softwaredesign/Puzzle.java
@@ -1,12 +1,10 @@
 package softwaredesign;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Puzzle {
     private Matrix matrix;
     private Sequences sequences;
-
     private String currPuzzle;
     private int bufferLen;
 
@@ -37,49 +35,17 @@ public class Puzzle {
     private String getPuzzleContent() {
         final int MIN_FILE_NUM = 1;
         final int MAX_FILE_NUM = 40;
+        FileReader fr = new FileReader();
         int fileNumber = getRandomNumber(MIN_FILE_NUM, MAX_FILE_NUM);
         String fileName = "puzzles/" + fileNumber + ".txt";
         String file = "";
         try {
-            file = readFile(fileName);
+            file = fr.readFile(fileName);
         }
         catch (IOException e){
             System.out.println(e.getMessage());
         }
         return file;
-    }
-
-    private String readFile(String fileName) throws IOException {
-        System.out.println("getResourcesAsStream : " + fileName);
-        InputStream is = getFileFromResourceAsStream(fileName);
-        String output = inputStreamToString(is);
-        return output;
-    }
-
-    private InputStream getFileFromResourceAsStream(String fileName) {
-
-        // The class loader that loaded the class
-        ClassLoader classLoader = getClass().getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(fileName);
-
-        // the stream holding the file content
-        if (inputStream == null) {
-            throw new IllegalArgumentException("file not found! " + fileName);
-        } else {
-            return inputStream;
-        }
-    }
-
-    public static String inputStreamToString(InputStream i) throws IOException {
-        String fileContent = "";
-        BufferedReader r = new BufferedReader(new InputStreamReader(i));
-        String l;
-        while((l = r.readLine()) != null) {
-            System.out.println("LINE: "+l);
-            fileContent += l + "\n";
-        }
-        i.close();
-        return fileContent;
     }
 
     public int getBufferLen(){
