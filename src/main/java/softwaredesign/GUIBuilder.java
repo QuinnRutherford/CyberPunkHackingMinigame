8f5133091bb792cde8e8c6b3771b412c89461e91
Fortanet A <antonioforcap@gmail.com>
2021-03-18 02:30:46 +0100

    Added temporary style to the GUIBuilder components.

--- src/main/java/softwaredesign/GUIBuilder.java
+++ src/main/java/softwaredesign/GUIBuilder.java
@@ -6,30 +6,37 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class GUIBuilder {
     private int timePerPuzzle;
+
     public GUIBuilder (final int timePerPuzzle) {
         this.timePerPuzzle = timePerPuzzle;
     }
 
-    public Scene sceneBuilder(GameManager gm, int timePerPuzzle) {
+    public Scene sceneBuilder(GameManager gm) {
         GridPane layoutPane = new GridPane();
+        layoutPane.setStyle("-fx-background-color: black;");
         GridPane matrixPane = matrixPaneBuilder(gm);
         GridPane sequencePane = sequencePaneBuilder(gm);
         GridPane timerPane = timerPaneBuilder();
+        timerPane.setStyle("-fx-background-color: black; -fx-border-color: green;");
+        sequencePane.setStyle("-fx-background-color: black; -fx-border-color: green;");
 
-        layoutPane.add(matrixPane, 0, 0);
-        layoutPane.add(sequencePane, 0, 1);
-        layoutPane.add(timerPane, 1, 0);
+        Label space = new Label();
+        space.setPrefHeight(1);
+        space.setPrefWidth(20);
+        layoutPane.add(space, 1, 0);
 
-        Scene scene = new Scene(layoutPane, 500, 500);
-        return scene;
+        layoutPane.add(timerPane, 0, 0);
+        layoutPane.add(matrixPane, 0, 1);
+        layoutPane.add(sequencePane, 2, 1);
+
+        return new Scene(layoutPane, 550, 350);
     }
 
-    public GridPane matrixPaneBuilder(GameManager gm) {
+    private GridPane matrixPaneBuilder(GameManager gm) {
         GridPane matrixPane = new GridPane();
 
         int matrixDim = gm.getCurrMatrixDims();
@@ -41,6 +48,7 @@ public class GUIBuilder {
                 //change button size
                 matrixButtons[col][row].setPrefWidth(50);
                 matrixButtons[col][row].setPrefHeight(50);
+                matrixButtons[col][row].setId("BUTTON");
                 final int ROW = row;
                 final int COL = col;
                 matrixButtons[col][row].setOnAction(e -> gm.AddElementToBuffer(ROW, COL));
@@ -50,41 +58,64 @@ public class GUIBuilder {
         return matrixPane;
     }
 
-    public GridPane sequencePaneBuilder(GameManager gm) {
+    private GridPane sequencePaneBuilder(GameManager gm) {
         GridPane sequencePane = new GridPane();
+        String textStyle = "-fx-text-fill: green; -fx-font-size: 16;";
+
+        Label titleLabel = new Label();
+        titleLabel.setText("SEQUENCE REQUIRED TO UPLOAD: \n");
+        titleLabel.setStyle(textStyle + "-fx-font-weight: bold;");
+        sequencePane.add(titleLabel, 0, 0);
 
         int numberOfSeq = gm.getCurrNumOfSeq();
         Label[] seqLabels = new Label[numberOfSeq];
         for(int n = 0; n < numberOfSeq; n++){
             String[] labelTxt = gm.getCurrNSeq(n);
-            String labelTxtCombined = "";
+            StringBuilder labelTxtCombined = new StringBuilder();
             for (int i = 0; i < labelTxt.length; i++) {
                 if (i == labelTxt.length-1) {
-                    labelTxtCombined += labelTxt[i];
+                    labelTxtCombined.append(labelTxt[i]);
                 } else {
-                    labelTxtCombined += labelTxt[i] + ", ";
+                    labelTxtCombined.append(labelTxt[i]).append("  ");
                 }
             }
-            seqLabels[n] = new Label(labelTxtCombined);
-            sequencePane.add(seqLabels[n], 0, n);
+            seqLabels[n] = new Label(labelTxtCombined.toString());
+            seqLabels[n].setPrefWidth(120);
+            seqLabels[n].setPrefHeight(50);
+            seqLabels[n].setStyle(textStyle);
+            sequencePane.add(seqLabels[n], 0, n + 1);
         }
         return sequencePane;
     }
 
-    public GridPane timerPaneBuilder() {
+    private GridPane timerPaneBuilder() {
         GridPane timerPane = new GridPane();
+        String textStyle = "-fx-text-fill: green; -fx-font-size: 16;";
+
+        Label titleLabel = new Label();
+        titleLabel.setText("BREACH TIME LEFT   ");
+        titleLabel.setStyle(textStyle + "-fx-font-weight: bold;");
 
         Label minutesTimer = new Label();
+        minutesTimer.setPrefWidth(50);
+        minutesTimer.setPrefHeight(50);
+        minutesTimer.setStyle(textStyle);
+
         Label secondsTimer = new Label();
-        timerPane.add(minutesTimer, 0,0);
-        timerPane.add(secondsTimer, 1, 0);
+        secondsTimer.setPrefWidth(50);
+        secondsTimer.setPrefHeight(50);
+        secondsTimer.setStyle(textStyle);
+
+        timerPane.add(titleLabel, 0, 0);
+        timerPane.add(minutesTimer, 1,0);
+        timerPane.add(secondsTimer, 2, 0);
 
         //Create animation
         Timeline timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
         KeyFrame frame = new KeyFrame(Duration.seconds(1), actionEvent -> {
             this.timePerPuzzle--;
-            String minutes = String.format("%02d", (timePerPuzzle % 3600)/60);
+            String minutes = String.format("%02d : ", (timePerPuzzle % 3600)/60);
             String seconds = String.format("%02d", timePerPuzzle % 60);
             minutesTimer.setText(minutes);
             secondsTimer.setText(seconds);
--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,40 +1,25 @@
 package softwaredesign;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
 
 public class Main extends Application {
-    static int timePerPuzzle = 10; //In seconds
-    private GameManager gm = new GameManager();
+    static int timePerPuzzle = 50; //In seconds
+    private final GameManager gm = new GameManager();
     
     public static void main (String[] args) {
-        //Thread gameThread = new Thread(new GameManager());
-        //gameThread.start();
         Application.launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
         GUIBuilder guiBuilder = new GUIBuilder(timePerPuzzle);
-        Scene scene = guiBuilder.sceneBuilder(this.gm, timePerPuzzle);
+        Scene scene = guiBuilder.sceneBuilder(this.gm);
 
-        //window creation
-        Stage window = primaryStage;
-        window.setResizable(false);
-        window.setTitle("CyberPunk Hacking Mini-game");
-        window.setScene(scene);
-        window.show();
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("CyberPunk Hacking Mini-game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
 }
